70931e6563b07da8b45f6ed2f8313f86381a339e
arifurrahmansajid <iamnobody3456@gmail.com>
2025-03-28 11:49:27 +0600

    update code for addCart removeCart inpout data from Api using fatch

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './Cart.css';
+
+const Cart = ({ cart, handleRemoveFromCart }) => {
+    return (
+        <div className='cart-container'>
+            {
+                cart.map(bottle => <div key={bottle.id}>
+                    <img src={bottle.img} alt="" />
+                    <button onClick={() => handleRemoveFromCart(bottle.id)}>x</button>
+                </div>)
+            }
+        </div>
+    );
+};
+
+export default Cart;
